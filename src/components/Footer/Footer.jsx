8868fe6fab0df98d4e7ca7a1ca24f040c8f6eb03
John Siordia <siohn@proton.me>
2024-12-02 15:28:02 -0600

    pre-submission commit -eslint config, Footer link edit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,21 @@ function Footer() {
     <footer className="footer">
       <p className="footer__copyright">© John Siordia 2024</p>
       <div className="footer__links">
-        <a href="https://www.pokemon.com/us" className="footer__link">
+        <a
+          href="https://www.pokemon.com/us"
+          className="footer__link"
+          target="_blank"
+        >
           Pokemon
         </a>
-        <a href="https://www.nintendo.com/us/" className="footer__link">
+        <a
+          href="https://www.nintendo.com/us/"
+          className="footer__link"
+          target="_blank"
+        >
           Nintendo
         </a>
-        <a href="https://pokeapi.co/" className="footer__link">
+        <a href="https://pokeapi.co/" className="footer__link" target="_blank">
           PokeAPI
         </a>
       </div>
